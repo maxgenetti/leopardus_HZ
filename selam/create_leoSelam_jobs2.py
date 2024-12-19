f056542f64b83555b517abd391a5ea786c996e25
maxgenetti <43384843+maxgenetti@users.noreply.github.com>
2024-12-19 14:27:41 -0800

    Update and rename create_leoSelam_jobs.py to create_leoSelam_jobs2.py
    Includes only suitability scaled simulations and lowered upper range of L. geoffroyi.

--- selam/create_leoSelam_jobs2.py
+++ selam/create_leoSelam_jobs2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 ne0_r = np.arange(0.005,0.1000001,0.0001)
-ne1_r = np.arange(0.005,0.2500001,0.0001)
+ne1_r = np.arange(0.005,0.2000001,0.0001)
 m_r =   np.arange(0.005,0.1000001,0.0001)
 
 j = 1
@@ -11,7 +11,7 @@ for i in list(range(0,5000)) :
     ne1 = str(round(np.random.choice(ne1_r),4))
     m = str(round(np.random.choice(m_r),4))
     print(f"{j}-{j+5}\t{ne0}\t{ne1}\t{m}")
-    for d in ['wedge','wedgeI','modern','wedgeS','wedgeIS','modernS']:
+    for d in ['wedgeS','wedgeIS','modernS']:
         out = open(f"./bash_jobs/{j}.sh", "w")
         out.write(f"python leoSELAM.py --ne0 {ne0} --ne1 {ne1} --mig {m} --demo {d} --model {j} 1>./results/model.{j}.tsv 2>./results/model.{j}.err\n")
         out.close()
